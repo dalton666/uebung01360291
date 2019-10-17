e4dcfab5a99946e084ffb07e0def35c067e06897
Timo Schmid <timo.schmid@gmx.at>
2019-10-17 15:33:28 +0200

    Methode erzeugt (binary2decimal)

--- src/main/java/Einstiegsbeispiel.java
+++ src/main/java/Einstiegsbeispiel.java
@@ -1,3 +1,20 @@
 public class Einstiegsbeispiel {
 
+    public int binary2decimal (int binary){
+        int dez = 0;
+        int i = 0;
+        while(true){
+            if(binary==0){
+                break;
+
+            }else{
+                int temp = binary%10;
+                dez += temp*Math.pow(2, i);
+                binary = binary/10;
+                i++;
+            }
+        }
+
+        return dez;
+    }
 }
--- src/test/java/EinstiegsbeispielTest.java
+++ src/test/java/EinstiegsbeispielTest.java
@@ -1,4 +1,7 @@
 public class EinstiegsbeispielTest {
-    
 
+
+    @org.junit.Test
+    public void binary2decimal() {
+    }
 }
